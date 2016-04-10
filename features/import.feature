9930c3db233f30775ca8356aac985da1c1e3db5a
Fred Collman <fredcollman@gmail.com>
2016-04-10 15:18:40 +0100

    Add passing scenario for duplicate transaction uploads

--- features/import.feature
+++ features/import.feature
@@ -1,5 +1,10 @@
 Feature: Users can import transactions from a file
 
 	Scenario: User imports from Santander
-		When I upload a .txt file from Santander
+		When I upload santander.txt
 		Then the most recent transactions should be displayed
+
+	Scenario: User imports duplicate transactions
+		Given I have previously uploaded santander.txt
+		When I upload santander.txt
+		Then 0 transactions should be uploaded
--- features/step_definitions/import_steps.rb
+++ features/step_definitions/import_steps.rb
@@ -1,9 +1,21 @@
-When(/^I upload a \.txt file from Santander$/) do
+def santander_upload
 	visit '/upload'
 	page.attach_file('transaction_file', Rails.root + 'features/support/santander.txt')
 	click_button 'Upload'
 end
 
+Given(/^I have previously uploaded santander.txt$/) do
+	santander_upload
+end
+
+When(/^I upload santander.txt$/) do
+	santander_upload
+end
+
 Then(/^the most recent transactions should be displayed$/) do
 	expect(page).to have_content('BANK GIRO CREDIT REF WORK')
+end
+
+Then(/^0 transactions should be uploaded$/) do
+	expect(page).to have_content('Uploaded 0 transactions')
 end
