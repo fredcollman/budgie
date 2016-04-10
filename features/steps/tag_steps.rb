4917288935f3b907312742655ab28978b0cbc1c5
Fred Collman <fredcollman@gmail.com>
2016-04-10 21:07:33 +0100

    Tweak feature steps to get it passing

--- features/steps/tag_steps.rb
+++ features/steps/tag_steps.rb
@@ -1,10 +1,10 @@
 When(/^I create a tag "([^"]*)"$/) do |tag|
   visit '/tags/new'
-  fill_in 'tag-name', with: tag
-  fill_in 'tag-description', with: 'A tag for testing'
+  fill_in 'tag[name]', with: tag
+  fill_in 'tag[description]', with: 'A tag for testing'
   click_button 'Save'
 end
 
 Then(/^I am taken to the "([^"]*)" tag page$/) do |tag|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_css('h1', tag)
 end
