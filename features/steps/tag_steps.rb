3305d53c063e9821485f7d51f28ba00989ce48cb
Fred Collman <fredcollman@gmail.com>
2016-04-11 16:02:31 +0100

    Add failing scenario for deleting tags

--- features/steps/tag_steps.rb
+++ features/steps/tag_steps.rb
@@ -13,3 +13,16 @@ end
 Then(/^I am taken to the "([^"]*)" tag page$/) do |tag|
   expect(page).to have_css('h1', tag)
 end
+
+Given(/^I am on the page for the "([^"]*)" tag$/) do |tag|
+  Tag.create! name: tag, description: 'Cucumber test tag'
+  visit "/tags/#{tag}"
+end
+
+When(/^I click the delete button$/) do
+  click_button 'Delete'
+end
+
+Then(/^the "([^"]*)" tag is no longer displayed$/) do |tag|
+  expect(page).not_to have_content(tag)
+end
--- features/tags.feature
+++ features/tags.feature
@@ -4,3 +4,8 @@ Feature: Users can add tags to transactions
 		Given I am on the home page
 		When I create a tag "amazon"
 		Then I am taken to the "amazon" tag page
+
+	Scenario: User deletes tag
+		Given I am on the page for the "rent" tag
+		When I click the delete button
+		Then the "rent" tag is no longer displayed
