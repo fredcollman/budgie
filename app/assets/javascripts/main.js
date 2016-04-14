6aaa1cd107c4fa38255b3755886ab9188f4a29aa
Fred Collman <fredcollman@gmail.com>
2016-04-14 21:03:48 +0100

    Bind event to document to avoid Turbolinks issues
    The event was binding to the links on page load, meaning that when the
    page changed (without calling a page load event) the bindings were
    lost.
    
    Fix is to use event delegation to bind directly to document and have
    this find the relevant element on-the-fly.
    The cancel button can safely be bound directly when the form is shown.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,17 @@
 $(function() {
-	$('.new-tag-btn').click(function() {
-		$(this).closest('tr').find('form').removeClass('hide');
-		$(this).addClass('hide');
-	});
-
-	$('.cancel-tag-btn').click(function() {
+	function cancelTagForm(event) {
+		event.preventDefault();
 		$(this).closest('form').addClass('hide');
 		$(this).closest('tr').find('.new-tag-btn').removeClass('hide');
-	});
+	}
+
+	function openTagForm(event) {
+		event.preventDefault();
+		$(this).closest('tr').find('form').removeClass('hide');
+		$(this).addClass('hide');
+
+		$('.cancel-tag-btn').click(cancelTagForm);
+	}
+
+	$(document).on('click', '.new-tag-btn', openTagForm);
 });
