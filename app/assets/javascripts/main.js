c930ad6ff1c319ffb1ad8562b117e8f158864fe4
Fred Collman <fredcollman@gmail.com>
2016-05-06 21:11:57 +0100

    Fix select to display correctly across page changes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,21 +1,28 @@
 $(function() {
-	function cancelTagForm(event) {
-		event.preventDefault();
-		$(this).closest('form').addClass('hide');
-		$(this).closest('tr').find('.new-tag-btn').removeClass('hide');
-	}
+	(function prepareTagForm() {
+		function cancelTagForm(event) {
+			event.preventDefault();
+			$(this).closest('form').addClass('hide');
+			$(this).closest('tr').find('.new-tag-btn').removeClass('hide');
+		}
 
-	function openTagForm(event) {
-		event.preventDefault();
-		$(this).closest('tr').find('form').removeClass('hide');
-		$(this).addClass('hide');
+		function openTagForm(event) {
+			event.preventDefault();
+			$(this).closest('tr').find('form').removeClass('hide');
+			$(this).addClass('hide');
 
-		$('.cancel-tag-btn').click(cancelTagForm);
-	}
+			$('.cancel-tag-btn').click(cancelTagForm);
+		}
 
-	$(document).on('click', '.new-tag-btn', openTagForm);
+		$(document).on('click', '.new-tag-btn', openTagForm);
+	})();
 
-	$(document).ready(function() {
-		$('select').material_select();
-	});
+	(function activateMaterializeSelect() {
+		function activeSelect() {
+			$('select').material_select();
+		}
+
+		$(document).ready(activeSelect);
+		$(document).on('page:load', activeSelect);
+	})();
 });
