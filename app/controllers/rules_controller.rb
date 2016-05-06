dfc11d42ac8d78e03464289146d15a2c8b5b28a2
Fred Collman <fredcollman@gmail.com>
2016-05-06 21:02:35 +0100

    Add create action for rules

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -6,4 +6,14 @@ class RulesController < ApplicationController
 		@rule = Rule.new
 		@rule_types = Rule.types
 	end
+
+	def create
+		@rule = Rule.create!(rule_params)
+		redirect_to action: 'index'
+	end
+
+private
+	def rule_params
+		params.require(:rule).permit(:name, :matching_regex, :tag_name)
+	end
 end
